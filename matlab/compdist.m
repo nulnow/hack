fe03cc472f5959ecd5f1a8cb526426cee88c6231
Noue Zoxinity <zoxinity@gmail.com>
2019-03-23 12:43:00 +0300

    simplification

--- matlab/compdist.m
+++ matlab/compdist.m
@@ -1,13 +1,16 @@
 function distvec = compdist(user1, user2)
 
-normalize = @(u) u/2 + 50;
+% normalize = @(u) u/2 + 50;
+% 
+% u1norm = normalize(user1);
+% u2norm = normalize(user2);
+% 
+% mlgs = @(x) 1./(1+exp(-x));
+% mlgsn = @(x) mlgs(x/10 - 5) * 100;
+% mlgs2d = @(x, y) mlgsn(min(x, y));
+% 
+% distvec = mlgs2d(u1norm, u2norm);
 
-u1norm = normalize(user1);
-u2norm = normalize(user2);
-
-mlgs = @(x) 1./(1+exp(-x));
-mlgsn = @(x) mlgs(x/10 - 5) * 100;
-mlgs2d = @(x, y) mlgsn(min(x, y));
-
-distvec = mlgs2d(u1norm, u2norm);
+logicsin = @(u1, u2) 50 * sin(pi*min(u1, u2)/200)+50;
+distvec = logicsin(user1, user2);
 end
--- matlab/myscr.m
+++ matlab/myscr.m
@@ -8,6 +8,7 @@ users = rand(n,k) .* 200 - 100;
 
 distvec = zeros(n, n, k);
 
+% вычисление вектора схожести
 for i = 1:n-1
     for j = i+1:n
         distvec(i, j, :) = compdist(users(i,:), users(j,:));
@@ -15,6 +16,19 @@ for i = 1:n-1
     end
 end
 
+% поиск максимумов векторов схожести
+pairs = [];
+for i = 1:n-1
+    for j = i+1:n
+        for e = 1:k
+            if distvec(i, j, e) > 80
+                pairs = [pairs; i j e distvec(i, j, e)];
+            end
+        end
+    end
+end
+
+% поверхности схожести
 x = 1:n;
 figure(1);
 surf(x, x, distvec(:, :, 1));
@@ -23,6 +37,7 @@ surf(x, x, distvec(:, :, 2));
 figure(3);
 surf(x, x, distvec(:, :, 3));
 
+% предпочтения пользователей u1, u2...
 y = 1:k;
 u1 = 1;
 u2 = 2;
@@ -30,6 +45,8 @@ figure(4);
 plot(y, users(u1,:), y, users(u2, :));
 axis([1,k,-100,100]);
 grid on;
+
+% ... и их вектор схожести
 figure(5);
 plot(y, squeeze(distvec(u1, u2, :)));
 axis([1,k,0,100]);
